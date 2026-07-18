b704e1908258b2edac1ad15700bde0202bd5e856
Saiyam-Sandhir-Jain <saiyam.sandhir.jain@gmail.com>
2026-07-18 21:34:48 +0530

    Arrays: Easy->Check if an array is sorted and rotated

--- src/main/java/Arrays/Easy.java
+++ src/main/java/Arrays/Easy.java
@@ -11,4 +11,32 @@ public class Easy {
 
         return max;
     }
+
+    public static int findSecondLargestElement(int[] arr) {
+        int largest = Integer.MIN_VALUE, secondLargest = Integer.MIN_VALUE;
+        for (int i : arr) {
+            if (i > largest) {
+                secondLargest = largest;
+                largest = i;
+            } else if (i < largest && i > secondLargest) {
+                secondLargest = i;
+            }
+        }
+
+        return secondLargest;
+    }
+
+    public static boolean checkIfSortedII(int[] arr) {
+        if (arr.length <= 2) return true;
+
+        int count = 0, i = 1;
+        while (i < arr.length) {
+            if (arr[i-1] > arr[i]) count++;
+            if (count > 1) return false;
+            i++;
+        } if (arr[i-1] > arr[0]) count++;
+
+        if (count > 1) return false;
+        return true;
+    }
 }
--- src/main/java/Arrays/Main.java
+++ src/main/java/Arrays/Main.java
@@ -11,12 +11,29 @@ public class Main {
         } System.out.println();
     }
     public static void main(String[] args) {
-        // Find largest element in the array
+        // Find largest element in an array
         { 
             System.out.println("Find largest element in an array:");
             int[] arr = {5, 3, -1, 0, 10, 4};
             printArr(true, arr);
             System.out.printf("The largest element in the given array is: %d\n", Easy.findLargestElement(arr));
+        } System.out.println();
+
+        // Find second largest element in an array
+        {
+            System.out.println("Find second largest element in an array:");
+            int[] arr = {5, 3, -1, 0, 10, 4};
+            printArr(true, arr);
+            System.out.printf("The second largest element in the given array is: %d\n", Easy.findSecondLargestElement(arr));
+        } System.out.println();
+
+        // Check if an array is sorted and rotated (leetcode: 1752)
+        {
+            System.out.println("Check if an array is sorted and rotated:");
+            int[] arr = {1, 2, 0, -1, 0};
+            printArr(true, arr);
+            String msg = (Easy.checkIfSortedII(arr)) ? "The given array is sorted and rotated" : "The given array is not sorted and rotated";
+            System.out.println(msg);
         } System.out.println();
     }
 }
